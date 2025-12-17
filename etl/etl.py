29f4ec4474b8340326141bbb0c06c693d10815ac
mkranjcev <kranjcev@srce.hr>
2025-12-17 15:40:42 +0100

    docker compose setup s etl-om, ordering kriv

---
+++ etl/etl.py
@@ -0,0 +1,27 @@
+import pyodbc
+
+conn = pyodbc.connect(
+    "DRIVER={ODBC Driver 18 for SQL Server};"
+    "SERVER=sqlserver;"
+    "DATABASE=master;"
+    "UID=sa;"
+    "PWD=Vjezbe123!;"
+    "TrustServerCertificate=yes;"
+)
+
+cursor = conn.cursor()
+# We are not guaranteed to run ETL AFTER the db is created.
+# So we connect to master db first,
+# and then wait for Northwind
+cursor.execute("USE Northwind")
+
+# Example ETL: add a row
+cursor.execute("""
+INSERT INTO Customers (CustomerID, CompanyName)
+VALUES ('ZZZZZ', 'ETL Insert Test')
+""")
+
+conn.commit()
+conn.close()
+
+print("ETL complete")
