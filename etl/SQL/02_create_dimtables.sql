ada69107e49f147c61a6e4606f6bfd32a99da7b4
mkranjcev <kranjcev@srce.hr>
2025-12-19 09:55:26 +0100

    dodaj dZaposlenici

--- etl/SQL/02_create_dimtables.sql
+++ etl/SQL/02_create_dimtables.sql
@@ -13,17 +13,32 @@ CREATE TABLE dProizvod (
     fProductID INT NOT NULL,
     imeProizvod VARCHAR(100) NOT NULL,
     sifKategorija INT NOT NULL,
-    imeKategorija VARCHAR(100) NOT NULL,
+    imeKategorija VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE dZaposlenik (
+    sifZaposlenik INT IDENTITY (1, 1) NOT NULL ,
+    -- originalna šifra - za spajanje pri punjenju činjeničnih
+    fEmployeeID INT NOT NULL,
+    imeZaposlenik VARCHAR(100) NOT NULL,
+    prezimeZaposlenik VARCHAR(100) NOT NULL,
+    sifMjestoZaposlenik INT NOT NULL,
 );
 
 --- Punjenje dimenzijskih tablica
 
 WITH mjesta AS (
-    SELECT DISTINCT ShipCity, ShipCountry
+    SELECT DISTINCT 
+    ShipCity AS City, ShipCountry AS Country
     FROM northwind.dbo.Orders
+    -- kod zaposlenika ima dodatnih gradova
+    UNION
+    SELECT DISTINCT
+        City, Country
+    FROM northwind.dbo.Employees
 )
 INSERT INTO dMjesto (imeGrad, imeDrzava)
-SELECT ShipCity, ShipCountry
+SELECT City, Country
 FROM mjesta;
 
 
@@ -40,3 +55,17 @@ WITH proizvodi AS (
 INSERT INTO dProizvod (fProductID, imeProizvod, sifKategorija, imeKategorija)
 SELECT fProductID, imeProizvod, sifKategorija, imeKategorija
 FROM proizvodi;
+
+WITH zaposlenici AS (
+    SELECT
+        EmployeeID AS fEmployeeID,
+        FirstName AS imeZaposlenik,
+        LastName AS prezimeZaposlenik,
+        dmj.sifMjesto AS sifMjestoZaposlenik
+    FROM northwind.dbo.Employees AS emp
+    LEFT JOIN dMjesto AS dmj 
+    ON emp.City = dmj.imeGrad
+)
+INSERT INTO dZaposlenik (fEmployeeID, imeZaposlenik, prezimeZaposlenik, sifMjestoZaposlenik)
+SELECT fEmployeeID, imeZaposlenik, prezimeZaposlenik, sifMjestoZaposlenik
+FROM zaposlenici;
