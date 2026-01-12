bd48ba215f390a0f8860968a9830d612caa874f6
mkranjcev <kranjcev@srce.hr>
2026-01-12 14:13:26 +0100

    ažuriraj rješenja zadataka s novim tablicama

--- etl/zadaci_SQL/zadatak_c2.sql
+++ etl/zadaci_SQL/zadatak_c2.sql
@@ -1,17 +1,16 @@
 --broj proizvoda koji su dobavljeni iz iste države u kojoj su i prodani u 2018. godini, prema državama
 
+
 SELECT 
-        dobava.Imedrzava,
+        proizvod.drzavaDobavljen,
         SUM(komad) AS broj_proizvoda
 FROM fProdajaProizvod
 JOIN dDatum
 ON fprodajaproizvod.sifdatumprodaja = ddatum.sifdatum
 JOIN dproizvod AS proizvod
 ON fprodajaproizvod.sifproizvod = proizvod.sifproizvod
-JOIN dmjesto AS dobava
-ON proizvod.sifmjestodobavljen = dobava.sifmjesto
-JOIN dmjesto AS prodano
-ON fprodajaproizvod.sifmjestoprodano = dobava.sifmjesto
+JOIN dkupac AS kupac
+ON fprodajaproizvod.sifkupac = kupac.sifkupac
 WHERE ddatum.godina = 1997
-AND dobava.imedrzava = prodano.imedrzava
-GROUP BY dobava.imedrzava;
+AND kupac.drzavaKupca = proizvod.drzavaDobavljen
+GROUP BY proizvod.drzavaDobavljen;
--- etl/zadaci_SQL/zadatak_c4.sql
+++ etl/zadaci_SQL/zadatak_c4.sql
@@ -1,14 +1,12 @@
 --usporedba ukupnih prihoda u 2017 godini u odnosu na državu u kojoj je zaposlenik
 
 SELECT 
-       zaposlenikMjesto.imedrzava,
+       dzaposlenik.drzavaZaposlenik,
        SUM(prihod) AS prihodi
 FROM fProdajaProizvod
 JOIN dDatum
 ON fprodajaproizvod.sifdatumprodaja = ddatum.sifdatum
 JOIN dzaposlenik
 ON fprodajaproizvod.sifzaposlenik = dzaposlenik.sifzaposlenik
-JOIN dMjesto AS zaposlenikMjesto
-ON dzaposlenik.sifmjestozaposlenik = zaposlenikMjesto.sifmjesto
 WHERE dDatum.godina = 1997
-GROUP BY zaposlenikMjesto.ImeDrzava;
+GROUP BY dzaposlenik.drzavaZaposlenik;
